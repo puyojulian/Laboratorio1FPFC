803732c2598047c574bf74e2839dbc267bd3d4da
Sebastián Orrego <orrego.sebastian@correounivalle.edu.co>
2023-09-11 21:59:09 -0500

    Casos de prueba y correcciones implementadas

--- src/main/scala/FuncionesRecursivas/package.scala
+++ src/main/scala/FuncionesRecursivas/package.scala
@@ -1,29 +1,11 @@
 package object FuncionesRecursivas {
-  // Ejemplo Profesor
-  def tamRRec(lista:List[Int]): Int = { // Referencia Recursiva
-    if (lista.isEmpty) 0
-    else 1 + tamRRec(lista.tail)
-  }
-
   // 1.1. Calcular el tamaño de una lista con un proceso iterativo
-  def tamRIter(lista: List[Int]): Int = { // No es iterativa.
-    def contItemsR(l: List[Int]) = 1 + tamRIter(l.tail)
-    if (lista.isEmpty) 0 else contItemsR(lista)
-  }
-
-  def tamRIterV2(lista: List[Int]): Int = {
-    def contItemsR(l: List[Int], cont: Int): Int = {
-      if (l.isEmpty) cont else contItemsR(l.tail, cont + 1)
+  def tamI(lista: List[Int]): Int = {
+    def contItems(l: List[Int], cont: Int): Int = {
+      if (l.isEmpty) cont else contItems(l.tail, cont + 1)
     }
-    contItemsR(lista, 0)
+    contItems(lista, 0)
   }
-
-  def ListaVacia(l: List[Int]): Int = if (l.isEmpty) 0 else 1
-  def tailLista(l: List[Int]) = if (l.tail.isEmpty) List() else l.tail
-  def tamIter(l: List[Int], cont: Int): Int = {
-    if (l.isEmpty) 0 else if (ListaVacia(tailLista(l)) > 0) tamIter(tailLista(l), cont + 1) else cont
-  }
-  def tamI(l: List[Int]) = tamIter(l, 1)
 
   // 1.2. Dividiento una lista en dos sublistas a partir de un pivote.
   def menoresQue(lista:List[Int], pivote:Int): List[Int] = {
@@ -54,24 +36,8 @@ package object FuncionesRecursivas {
   }
 
   // 1.3. Calculando el k-esimo elemento de una lista
-  def k_elem(lista: List[Int], k: Int): Int = {
-    def encontrarValorK(l: List[Int]): Int = {
-      if (!l.isEmpty)
-        if (tamRIter(menoresQue(l,l.head)) == (k - 1))
-          l.head
-        else {
-          if(l.tail.isEmpty)
-            l.head
-          else
-            encontrarValorK(l.tail)
-      } else -1
-    }
-    encontrarValorK(lista)
-  }
-
-  def k_elem2(l: List[Int], k: Int): Int = {
+  def k_elem(l: List[Int], k: Int): Int = {
     val c: Int = k
-
     def k_elemCons(l: List[Int], c: Int, k: Int): Int = {
       if (!l.isEmpty) {
         val tamMenores: Int = tamI(menoresQue(l, c))
